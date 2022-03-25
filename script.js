cd86060df5ca9a46142c06cefb5a446eb6540093
gustavosgithub <101032048+gustavosgithub@users.noreply.github.com>
2022-03-25 01:44:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//ARRAY COM OS CONTEÚDOS
 let musicas = [
     {titulo:'Dry Country', artista:'Bon Jovi', src:'audio/Dry Country.mp3',
     img:'capas/KeepTheFaith.jpg'},
@@ -6,25 +7,20 @@ let musicas = [
     {titulo:'What About Now', artista:'Bon Jovi', src:'audio/What About Now.mp3',
     img:'capas/whatAboutNow.webp'}
 ]
-
+//NOMEANDO AS VARIÁVEIS
 let musica = document.querySelector('audio');
 let indexMusica = 0;
-
 let duracaoMusica= document.querySelector('.fim')
 let imagem = document.querySelector('.capa')
 let nomeMusica = document.querySelector('.descricao h2');
 let nomeArtista = document.querySelector('.descricao i');
 
+//DESENCADEANDO EVENTOS
 renderizarMusica(indexMusica)
-
 window.onload = duration;
-
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
-
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
-
 musica.addEventListener('timeupdate', atualizarBarra);
-
 document.querySelector('.anterior').addEventListener('click', () =>{
     indexMusica--;
     if(indexMusica<0){
@@ -39,6 +35,8 @@ document.querySelector('.proxima').addEventListener('click', () =>{
     }
     renderizarMusica(indexMusica);
 });
+
+//FUNÇÕES
 function renderizarMusica(index){
     musica.setAttribute('src', musicas[index].src);
     musica.addEventListener('loadeddata', ()=>{
@@ -49,26 +47,22 @@ function renderizarMusica(index){
     segundoParaMinutos(Math.floor(musica.duration));
     });
 }
-
 function tocarMusica(){
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
     document.querySelector('.botao-play').style.display = 'none';
 }
-
 function pausarMusica(){
     musica.pause();
     document.querySelector('.botao-pause').style.display = 'none';
     document.querySelector('.botao-play').style.display = 'block';
 }
-
 function atualizarBarra(){
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%' ;
     let tempoDecorrido = document.querySelector('.inicio')
     tempoDecorrido.textContent =  segundoParaMinutos(Math.floor(musica.currentTime))
 }
-
 function segundoParaMinutos(segundos){
     let campoMinutos = Math.floor(segundos/60);
     let campoSegundos = segundos % 60;
@@ -77,7 +71,6 @@ function segundoParaMinutos(segundos){
     }
     return campoMinutos + ':' +campoSegundos;
 }
-
 function duration(){
     duracaoMusica.textContent = 
     segundoParaMinutos(Math.floor(musica.duration));
